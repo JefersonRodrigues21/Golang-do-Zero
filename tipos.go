8ff1a564591f81f2b10433e8dc5c251089c45cce
JefersonRodrigues21 <dev.geekjs@gmail.com>
2024-07-01 22:53:32 -0300

    primeiros passos

--- tipos.go
+++ tipos.go
@@ -1,20 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// bool (true/false)
-	// fmt.Printf("Type: %T - Value: %v\n", false, false)
+	 fmt.Printf("Type: %T - Value: %v\n", false, false)
 
 	// string - sequência de bytes
-	fmt.Printf("Type: %T - Value: %v\n", "steph123", "steph123")
-	fmt.Printf("Type: %T - Value: %v\n", "1", "1")
-
-	// int
-	fmt.Printf("Type: %T - Value: %v\n", 1, 1)
-
-	// float (float64/float32) - decimal
-	fmt.Printf("Type: %T - Value: %v\n", 1.233, 1.233)
+    // fmt.Printf("Type: %T - Value: %v\n", "steph123", "steph123")
+    // fmt.Printf("Type: %T - Value: %v\n", "1", "1")
+ 
+    // int
+    // fmt.Printf("Type: %T - Value: %v\n", 1, 1)
+ 
+    // float (float64/float32) - decimal
+    // fmt.Printf("Type: %T - Value: %v\n", 1.233, 1.233)
 }
 
 // Tipos:
